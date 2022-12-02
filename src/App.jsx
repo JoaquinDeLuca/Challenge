56d1110959a2f759d68eedc2ab89e70e93d815c0
JoaquinDeLuca <joaqui222@gmail.com>
2022-12-02 18:13:12 -0300

    add componente Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Login from './Components/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <p>Hola mundo!</p>
+    <div className='flex justify-center m-10'>
+      <Login/>
     </div>
   )
 }
