1ee42f0dfc535e03b30a96f6bbc393684f7383d2
JoaquinDeLuca <joaqui222@gmail.com>
2022-12-12 15:16:07 -0300

    add Details

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './Components/Login';
 import List from './Components/list';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Details from './Components/Details'
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>} />
         <Route path='/listado' element={<List />} />
+        <Route path='/detalle/:id' element={<Details />}/>
       </Routes>
       <Footer/>
     </>
