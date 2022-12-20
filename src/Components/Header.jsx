53202151a2100f36a149dd2fa0a055bf250435c1
JoaquinDeLuca <joaqui222@gmail.com>
2022-12-20 19:05:18 -0300

    is left on sticky

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import Search from "./Search";
+import favPng from '../assets/favPos.png'
 
 
 export default function Header(props) {
 
   return (
     <>
-        <header className="bg-neutral-900 flex justify-center text-white h-min-[6vh]">
+        <header className="bg-neutral-900 flex justify-center text-white h-min-[6vh] sticky top-0 z-10">
             <nav className="container flex flex-col gap-3 mt-4 sm:flex-row sm:mt-0 items-center justify-around">
                 <ul className="flex gap-4">
                     <li>
@@ -19,7 +20,7 @@ export default function Header(props) {
                         <Link to='/contacto'>Contacto</Link>
                     </li>
                     <li>
-                        <Link className="flex items-center" to='/favoritos'>Favoritos {props.favorites.length > 0  && <p className="ml-2">{props.favorites.length}🖤</p>}</Link>
+                        <Link className="flex items-center" to='/favoritos'>Favoritos {props.favorites.length > 0  && <> <p className="ml-2">{props.favorites.length}</p> <img className="w-[22px] h-[22px]" src={favPng} alt="favoritos"/> </> }</Link>
                     </li>
                 </ul>
                 <Search />
