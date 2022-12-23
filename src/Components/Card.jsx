e1ff7a9259b34cad5d3f22557b136924267112b4
JoaquinDeLuca <joaqui222@gmail.com>
2022-12-22 23:18:10 -0300

    add conditional to img

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,31 +1,13 @@
-import React, { useEffect, useState } from 'react'
 import favPng from '../assets/favPos.png'
-import favNeg from '../assets/favNeg.png' 
-
 import { Link } from "react-router-dom"
 
-
 export default function Card(props) {
-
-    const[removeFav, setRemoveFav] = useState(favNeg)
-    const[addFav, setAddFav] = useState(favPng)
-
-    // const a = () => {
-    //     const favMovie = localStorage.getItem('favs')
-    //     let favParse = JSON.parse(favMovie)
-    //     if(!props.moviesList.includes(favParse)){
-    //        return removeFav
-    //     }else{
-    //        return addFav
-    //     }
-
-    // }
 
   return (
     <div className="px-4 my-6 container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-4  gap-4 m-auto bg-neutral-200 p-4 rounded-lg">
     {props.moviesList.map( movie => {
         return(
-            <div key={movie.id} className="flex flex-col bg-neutral-700 rounded-lg shadow-xl">
+            <div key={movie.id} className="flex flex-col bg-neutral-700 rounded-lg shadow-xl"> 
                     <button 
                         onClick={props.addOrRemoveFavorites}
                         data-movieid = {movie.id}
@@ -33,7 +15,7 @@ export default function Card(props) {
                         <img src={favPng} alt="favorito" />
                     </button>
                 <div>
-                    <img id='imgMovie' className="rounded-t-lg" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+                    <img id='imgMovie' className="rounded-t-lg" src={movie.imgURl || `https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
                     <div className="h-min-[220px] flex flex-col items-center p-3 gap-2 ">
                         <h5 className="text-white text-lg font-black">{movie.title}</h5>
                         <p className="text-white">{movie.overview.substring(0,110)}...</p>
